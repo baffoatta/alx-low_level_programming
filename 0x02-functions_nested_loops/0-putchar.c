872a246b15d72a900641004edb34a4cfc63e9e35
baffoatta <baffoatta@gmail.com>
2023-02-21 15:14:49 -0800

    changes

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: 0
+**/
+
+int main(void)
+{
+	
+	char  s[] = "_putchar";
+	
+	for (i = 0; i <= sizeof(s); i++)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
