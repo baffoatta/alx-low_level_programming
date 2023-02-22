f841ab014c4c90d95ec77c6dfdd34af06ce47c66
baffoatta <baffoatta@gmail.com>
2023-02-21 16:30:31 -0800

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
 
 int main(void)
 {
-
-	char s[] = "_putchar";
-	
-	for (i = 0; i <= 7; i++)
-	{
-		_putchar(s[i]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
